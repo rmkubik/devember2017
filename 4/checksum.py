c9c5f9589aaff482fef141bdf0045696c35807df
rmkubik <rmkubik@me.com>
2017-12-04 08:48:22 -0800

    Checksum each row

--- 4/checksum.py
+++ 4/checksum.py
@@ -1,6 +1,19 @@
 import csv
+import math
+
+def checkSumRow(row):
+    max = -math.inf
+    min = math.inf
+    for numberString in row:
+        number = int(numberString)
+        if (number > max):
+            max = number
+        if (number < min):
+            min = number
+    return max - min
+
 
 with open('data.tsv', 'r') as dataFile:
     reader = csv.reader(dataFile, delimiter='\t')
     for row in reader:
-        print(row)
+        print(checkSumRow(row))
