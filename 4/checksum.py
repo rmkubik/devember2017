fe7a4372d28e7e3aa4e90d4386c03ceda636c652
rmkubik <rmkubik@me.com>
2017-12-04 20:32:36 -0800

    Second  star for advent of code day 2

--- 4/checksum.py
+++ 4/checksum.py
@@ -1,7 +1,7 @@
 import csv
 import math
 
-def checkSumRow(row):
+def checkSumRowMinMax(row):
     max = -math.inf
     min = math.inf
     for numberString in row:
@@ -13,9 +13,22 @@ def checkSumRow(row):
     return max - min
 
 
+def checkSumEvens(row):
+    for i in range(len(row) - 1):
+        dividend = int(row[i])
+        for divisorString in row[i+1:]:
+            divisor = int(divisorString)
+            if (dividend % divisor == 0):
+                return dividend / divisor
+            if (divisor % dividend == 0 ):
+                return divisor / dividend
+
 with open('data.tsv', 'r') as dataFile:
     reader = csv.reader(dataFile, delimiter='\t')
-    sum = 0
+    sumMinMax = 0
+    sumEvens = 0
     for row in reader:
-        sum += checkSumRow(row)
-    print(sum)
+        sumMinMax += checkSumRowMinMax(row)
+        sumEvens += checkSumEvens(row)
+    print(sumMinMax)
+    print(sumEvens)
