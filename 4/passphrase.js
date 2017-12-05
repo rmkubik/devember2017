6b1cfe48f7643b8f400836661b70d562ee9ef980
rmkubik <rmkubik@me.com>
2017-12-04 22:41:29 -0800

    Detect anagrams for AoC day #4 part 2

--- 4/passphrase.js
+++ 4/passphrase.js
@@ -18,4 +18,21 @@ const validPassPhrases = passphrases.split('\n').filter((passphrase) => {
     return isPassphraseValid(passphrase.trim());
 });
 
+const isPassphraseValid2 = (passphrase) => {
+    const words = new Set();
+    for (word of passphrase.split(' ')) {
+        if (words.has(word.split('').sort().join(''))) {
+            return false;
+        } else {
+            words.add(word.split('').sort().join(''));
+        }
+    }
+    return true;
+}
+
+const validPassPhrases2 = passphrases.split('\n').filter((passphrase) => {
+    return isPassphraseValid2(passphrase.trim());
+});
+
 console.log(validPassPhrases.length);
+console.log(validPassPhrases2.length);
