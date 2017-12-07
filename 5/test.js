59425bfc7954d8f9386e8da1a3073afe126632d2
rmkubik <rmkubik@me.com>
2017-12-06 22:34:02 -0800

    remove unused test files

--- 5/test.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var array = [1, 2, 3];
-console.log(array);
-array = [];
-console.log(array);
--- 5/test.ts
+++
@@ -1,4 +0,0 @@
-let array: number[] = [1, 2, 3];
-console.log(array);
-array = []
-console.log(array);
