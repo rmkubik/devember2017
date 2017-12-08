12f185b3f7f69f3b87b59c12ed1c48842d398733
rmkubik <rmkubik@me.com>
2017-12-08 00:18:45 -0800

    Port demo to this project file

--- 7/firebase-chat/src/App.js
+++ 7/firebase-chat/src/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import fb from './firebase';
 
 class App extends Component {
+  state = {
+    messages: []
+  }
+
+  componentWillMount() {
+    let messagesRef = fb.database().ref('messages').orderByKey().limitToLast(100);
+    messagesRef.on('child_added', snap => {
+      let message = { text: snap.val(), id: snap.key }
+      this.setState(prevState => {
+        return {messages: [message].concat(prevState.messages)};
+      })
+    })
+  }
+
+  addMessage = (e) => {
+    e.preventDefault();
+
+    fb.database().ref('messages').push( this.inputEl.value );
+    this.inputEl.value = '';
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form onSubmit={this.addMessage.bind(this)}>
+          <input type="text" ref={ el => this.inputEl = el } />
+          <input type="submit" />
+          <ul>
+            {
+              this.state.messages.map(message => <li key={message.id}>{message.text}</li>)
+            }
+          </ul>
+        </form>
       </div>
     );
   }
